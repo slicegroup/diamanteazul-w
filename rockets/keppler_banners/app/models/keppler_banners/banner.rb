5e3c0b82226ef44acd37cfe33927e76a1379a8e7
maria <castrogonzalezmariajose6@gmail.com>
2019-07-26 15:35:56 -0400

    Module banners funcional

--- rockets/keppler_banners/app/models/keppler_banners/banner.rb
+++ rockets/keppler_banners/app/models/keppler_banners/banner.rb
@@ -11,9 +11,17 @@ module KepplerBanners
     include Searchable
     acts_as_list
     acts_as_paranoid
+    validates_presence_of :title, :subtitle
+    validate :check_cta, on: [:create, :edit]
 
     def self.index_attributes
       %i[title subtitle link]
+    end
+
+    def check_cta
+      if cta && link.eql?('')
+        errors.add :link, 'Debe introducir un link al CTA'
+      end
     end
   end
 end
