edae928887b4b939dbfcec797bf3f0e2787d2a18
mariajosecg <castrogonzalezmariajose6@gmail.com>
2019-09-23 13:59:21 -0400

    Added mailers

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,14 @@
 class ContactMailer < ApplicationMailer
 	default from: 'ventas@joyeriadiamanteazul.xyz'
 
-  def cotization_admin(cotization)
+  def cotization_admin(cotization, product)
     @cotization = cotization
+    @product = product
     mail(from: 'ventas@joyeriadiamanteazul.xyz', to: 'ventas@joyeriadiamanteazul.xyz', subject: 'Solicitud de cotización')
-  end
+  end	
 
-  def shopy_client(cotization)
-    @cotization = cotization
+  def shopy_client(product)
+    @product = product
     mail(from: 'ventas@joyeriadiamanteazul.xyz', to: 'ventas@joyeriadiamanteazul.xyz', subject: 'Solicitud de compra')
   end
 end
